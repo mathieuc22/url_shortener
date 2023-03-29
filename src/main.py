1f933a087ecccdc2a01c2121c260ec2ec07dcc58
Mathieu <mathieu.collet@gmail.com>
2023-03-29 11:40:01 +0200

    Add S3 for prod

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 import json
 import os
 import string
-from cgitb import handler
+from pathlib import Path
 from secrets import choice
 from typing import List
 
+import boto3
+from botocore.exceptions import ClientError
 from fastapi import FastAPI, HTTPException
 from fastapi.openapi.utils import get_openapi
 from mangum import Mangum
-from pydantic import BaseModel
-from pathlib import Path
+from pydantic import BaseModel, parse_obj_as
 
-env = os.getenv("ENV", "prod")
+ENV = os.environ.get("APP_ENV", "dev")
 
 app = FastAPI(
     title="URL Shortener",
@@ -19,7 +20,17 @@ app = FastAPI(
     version="1.0.0",
 )
 
-DATA_FILE = Path(__file__).parent.resolve() / "urls.json"
+FILE_NAME = "urls.json"
+DATA_FILE = Path(__file__).parent.resolve() / FILE_NAME
+ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+BUCKET_NAME = os.getenv("AWS_S3_BUCKET_NAME")
+
+s3 = boto3.client(
+    service_name="s3",
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=SECRET_KEY,
+)
 
 
 class UrlEntry(BaseModel):
@@ -27,7 +38,7 @@ class UrlEntry(BaseModel):
     url: str
 
 
-def read_data():
+def read_data_local():
     try:
         with open(DATA_FILE, "r") as f:
             data = json.load(f)
@@ -36,11 +47,44 @@ def read_data():
         return []
 
 
-def write_data(data: List[UrlEntry]):
+def write_data_local(data: List[UrlEntry]):
     with open(DATA_FILE, "w") as f:
         data_as_dicts = [entry.dict() for entry in data]
         json_data = json.dumps(data_as_dicts, indent=2)
         f.write(json_data)
+
+
+def read_data_s3() -> List[UrlEntry]:
+    try:
+        response = s3.get_object(Bucket=BUCKET_NAME, Key=FILE_NAME)
+        content = response["Body"].read()
+        data = json.loads(content)
+        return parse_obj_as(List[UrlEntry], data)
+    except ClientError as e:
+        print("Error reading data from S3:", e)
+        return []
+
+
+def write_data_s3(data: List[UrlEntry]) -> None:
+    try:
+        serialized_data = json.dumps([entry.dict() for entry in data], indent=2)
+        s3.put_object(Bucket=BUCKET_NAME, Key=FILE_NAME, Body=serialized_data)
+    except ClientError as e:
+        print("Error writing data to S3:", e)
+
+
+def read_data() -> List[UrlEntry]:
+    if ENV == "prod":
+        return read_data_s3()
+    else:
+        return read_data_local()
+
+
+def write_data(data: List[UrlEntry]) -> None:
+    if ENV == "prod":
+        write_data_s3(data)
+    else:
+        write_data_local(data)
 
 
 def generate_id(size=5):
@@ -113,5 +157,5 @@ def custom_openapi():
 app.openapi = custom_openapi
 
 
-if env == "prod":
+if ENV == "prod":
     handler = Mangum(app=app)
